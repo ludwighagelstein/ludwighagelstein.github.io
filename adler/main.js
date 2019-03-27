75824e1cf36cffd788aa74df6296e23f3200d0d6
Ludwig Hagelstein <ludwighagelstein@ydWLI-U1-33-207.uibk.ac.at>
2019-03-27 18:52:01 +0100

    Adlerblicke extern und MapView auf alle Pins

--- adler/main.js
+++ adler/main.js
@@ -16,27 +16,13 @@ const titel2 = div.getAttribute("data-title2");
 // Karte initialisieren
 let karte = L.map("map"); //neue Konstante "karte", mit L. öffne ich die Leaflet Bibliothek
 //console.log(map);
-var markers = [
-    L.marker([breite1, laenge1]),
-    L.marker([breite2, laenge2])
-]
 
-var group = L.featureGroup(markers).addTo(karte);
 
-/*setTimeout(function () {
-    karte.fitBounds(group.getBounds());
-}, 0);*/
-
-/*karte.setView(function () {
-    karte.fitBounds(group.getBounds());
-}, 12
-);*/
-
-//Auf Ausschnitt zoomen
+/*Auf Ausschnitt zoomen #wird unten besser gemacht!
 karte.setView(
     [47.2, 11.2],
     8
-); //[] Liste aus einträgen & Zoomlevel (1=ganze Welt)
+); //[] Liste aus einträgen & Zoomlevel (1=ganze Welt)*/
 
 //OpenstreetMap einbauen
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
@@ -55,38 +41,20 @@ let pin2 = L.marker(
 pin1.bindPopup(titel1).openPopup(); //In die Klammern könnte man wieder ganz normal HTML Markup reinschreiben
 pin2.bindPopup(titel2).openPopup();
 
+let blickeGruppe = L.featureGroup().addTo(karte);
 
-const adlerblicke = [{
-        kunde: "Wilder Kaiser",
-        standort: "Gruttenhütte",
-        seehoehe: 1640,
-        lat: 47.55564,
-        lng: 12.31861
-    },
-    {
-        kunde: "Bergbahn Scheffau ",
-        standort: "Brandstadl",
-        seehoehe: 1640,
-        lat: 47.4912,
-        lng: 12.248
-    },
-    {
-        kunde: "Lechtal Tourismus ",
-        standort: "Sonnalm Jöchelspitze",
-        seehoehe: 1786,
-        lat: 47.27528,
-        lng: 10.36505
-    }
-]; //JS Objekt, An schlüssel (karte) kann ich Wertepaare anhängen - Objekt ist alleinstehend, d.h. man kann auch Werte von außerhalb "wiederholen" 
-
-for (let blick of adlerblicke) { //let kann überschireben weren!
-    let blickpin = L.marker( 
+for (let blick of ADLERBLICKE) { //let kann überschireben weren!
+    let blickpin = L.marker(
         [blick.lat, blick.lng]
-    ).addTo(karte);
+    ).addTo(blickeGruppe);
     blickpin.bindPopup(
         `<h1> ${blick.standort}</h1>
             <p> ${blick.seehoehe}</p>
             <em> ${blick.kunde}</em>`
     )
-    console.log(blick); //ausgeben, was jeweil in  der Variable blick steht (Die existiert nur innerhalb des loops!)
+    //console.log(blick); //ausgeben, was jeweil in  der Variable blick steht (Die existiert nur innerhalb des loops!)
+    //console.log(ADLERBLICKE);
 } //Für jedes von diesen Elementen in adlerblicke soll 1 Pin an der Stelle lat und lng für diese variable blick gesetzt werden
+
+console.log(blickeGruppe.getBounds());
+karte.fitBounds(blickeGruppe.getBounds()); // Setzt den map Extent genau auf die ausweitung von den Markern
