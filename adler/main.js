cc9addb758d83d84caf6df8c6f4168a054a61bb4
Ludwig Hagelstein <ludwighagelstein@ydWLI-U1-33-207.uibk.ac.at>
2019-03-27 15:53:21 +0100

    main und js updaten

---
+++ adler/main.js
@@ -0,0 +1,30 @@
+// alert("Hoi");
+
+
+const div = document.getElementById("map"); //Ich definiere die Konstante div und das div element ist drinnen
+const breite = div.getAttribute("data-lat");
+const laenge = div.getAttribute("data-lng");
+const titel = div.getAttribute("data-title");
+
+//console.log("Breite=",lat,"Länge=",lng,"Titel=",title);
+
+// Karte initialisieren
+let karte = L.map("map"); //neue Konstante "karte", mit L. öffne ich die Leaflet Bibliothek
+//console.log(map);
+
+//Auf Ausschnitt zoomen
+karte.setView(
+[breite,laenge],
+13
+);  //[] Liste aus einträgen & Zoomlevel (1=ganze Welt)
+
+//OpenstreetMap einbauen
+L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+
+//Positionsmarker einfügen
+let pin = L.marker(
+    [breite,laenge]
+).addTo(karte);
+
+// Popup zum Pin hängen
+pin.bindPopup(titel).openPopup(); //In die Klammern könnte man wieder ganz normal HTML Markup reinschreiben
