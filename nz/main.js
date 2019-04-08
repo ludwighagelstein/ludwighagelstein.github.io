95beca5d8f47a517ade2f29f9ce7359df74b3c72
Ludwig Hagelstein <ludwighagelstein@Ludwigs-MacBook-Pro.local>
2019-04-08 16:14:48 +0200

    karte pimpen

--- nz/main.js
+++ nz/main.js
@@ -18,8 +18,41 @@ karte.setView(
 13
 );  //[] Liste aus einträgen & Zoomlevel (1=ganze Welt)
 
-//OpenstreetMap einbauen
-L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(karte);
+//Maps einbauen
+
+const kartenLayer = {
+    osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+        subdomains: ["a", "b", "c"],
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    }),
+    stamen_toner: L.tileLayer("http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png",{
+        subdomains: ['a','b','c'],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    stamen_terrain: L.tileLayer("http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg",{
+        subdomains: ['a','b','c'],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+    }),
+    stamen_watercolor: L.tileLayer("http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg",{
+        subdomains: ['a','b','c'],
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
+    }),
+}
+
+
+
+kartenLayer.osm.addTo(karte);
+
+//Auswahlmenü hinzufügen
+L.control.layers({
+    "OpenStreetMap" : kartenLayer.osm,
+    "Stamen Toner": kartenLayer.stamen_toner,
+    "Stamen Terrain": kartenLayer.stamen_terrain,
+    "Stamen Watercolor": kartenLayer.stamen_watercolor
+}).addTo(karte);
+
+//Fullscreen
+karte.addControl(new L.Control.Fullscreen());
 
 //Positionsmarker einfügen
 let pin = L.marker(
@@ -28,3 +61,17 @@ let pin = L.marker(
 
 // Popup zum Pin hängen
 pin.bindPopup(titel).openPopup(); //In die Klammern könnte man wieder ganz normal HTML Markup reinschreiben
+
+//Koordinaten in Adresszeile einfügen
+var hash = new L.Hash(karte);
+
+//Koordinaten bei Maus hinzufügen
+var coords = new L.Control.Coordinates(); // you can send options to the constructor if you want to, otherwise default values are used
+
+coords.addTo(karte);
+
+karte.on('click', function(e) {
+	coords.setCoordinates(e);
+});
+
+console.log(coords);
